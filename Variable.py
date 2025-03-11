b57090d24db3423ca600fe640cbfea9db9670809
Croxy12321 <2766197709@qq.com>
2025-03-11 10:30:46 +0800

    Update Variable.py

--- Variable.py
+++ Variable.py
@@ -10,3 +10,14 @@ else:
     print("driving is faster")
 print(f"bus：{c}min")
 print(f"driving：{f}min")
+
+X = True
+Y = False
+W = X and Y
+print(f"X: {X}, Y: {Y}, W (X and Y): {W}")
+print("X\tY\tW (X and Y)")
+print(f"{True}\t{True}\t{True and True}")
+print(f"{True}\t{False}\t{True and False}")
+print(f"{False}\t{True}\t{False and True}")
+print(f"{False}\t{False}\t{False and False}")
+print("the first ten triangular number：")
